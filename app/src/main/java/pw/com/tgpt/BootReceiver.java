f88644a43e2337cdc644390ca9a6aa5ab6d965c7
pwoo <patrick.woo@gmail.com>
2015-05-12 22:53:46 -0400

    Commit latest changes.

--- app/src/main/java/pw/com/tgpt/BootReceiver.java
+++ app/src/main/java/pw/com/tgpt/BootReceiver.java
@@ -9,11 +9,17 @@ public class BootReceiver extends BroadcastReceiver {
     private static final String TAG = "TGPT";
 
     public BootReceiver() {
+        super();
+        Log.d(TAG, "BootReceiver");
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "Boot message received!");
         // TODO: Start service that sends current price to notifications
+        Context appContext = context.getApplicationContext();
+        Intent i = new Intent(appContext, PushUpdateService.class);
+        i.setAction("pw.com.tgpt.action.FOO");
+        appContext.startService(i);
     }
 }
---
+++ app/src/main/java/pw/com/tgpt/PushUpdateService.java
@@ -0,0 +1,125 @@
+package pw.com.tgpt;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class PushUpdateService extends IntentService {
+    private static final String TAG = "TGPT";
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static Semaphore serviceMutex = new Semaphore(1, true);
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    private static final String ACTION_FOO = "pw.com.tgpt.action.FOO";
+    private static final String ACTION_BAZ = "pw.com.tgpt.action.BAZ";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "pw.com.tgpt.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "pw.com.tgpt.extra.PARAM2";
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, PushUpdateService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, PushUpdateService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    public PushUpdateService() {
+        super("PushUpdateService");
+        Log.v(TAG, "PushUpdateService started");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.w(TAG, "PushUpdateService destroyed!");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+//        while (true) {
+            try {
+                serviceMutex.acquire();
+                if (intent != null) {
+                    final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                    final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                    final Context appContext = this.getApplicationContext();
+                    final Runnable notification = new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.v(TAG, "onHandleIntent");
+                            NotificationCompat.Builder n = new NotificationCompat.Builder(appContext);
+                            n.setSmallIcon(R.mipmap.ic_launcher);
+                            n.setContentText("hello world!");
+                            n.setContentTitle("My notification!");
+
+                            NotificationManager notifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                            notifyMgr.notify(123, n.build());
+                        }
+                    };
+                    notification.run();
+                }
+                serviceMutex.release();
+            }
+            catch (InterruptedException e) {
+
+            }
+//        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
